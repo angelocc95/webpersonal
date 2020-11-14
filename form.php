81855e6b906bac035e0357effd8d61e9363ba820
angelocc95 <angelo.cc.95@gmail.com>
2020-11-14 00:30:56 -0500

    modificacion en css,form.php y agregacion de enviar.html

--- form.php
+++ form.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+    <meta name="description" content="my web">
+    <meta name="keywords" content="HTML5,CSS3, JavaScript">
+    <!-- fonst awesome iconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <!-- font oswald -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400&display=swap"> 
+    <link rel="stylesheet" href="./css/estilo.css">
     <title>Datos</title>
-</head>
+</head> 
 <body>
+    <section class="titulo-form">
     <h1>Tus Datos</h1>
     <p>Estos son los datos que nos has enviado:</p>
-
+    <br>
     <?php
-    $n=$_POST["name"];
-    $e=$_POST["email"];
-    $c=$_POST["coment"];
 
-    echo("Nombre: ". $_POST['name']);
-    echo("</br>");
-    echo("Correo Electronico: ". $_POST["email"]);
-    echo("</br>");
-    echo("Tu comentario: ". $_POST["message"]);
-    echo("</br>");
+    echo("Nombre: ". $_POST['name']);echo("</br>");
+    echo("Correo Electronico: ". $_POST["email"]);echo("</br>");
+    echo("Tu comentario: ". $_POST["message"]);echo("</br>");
     
     ?>
-    <p>Comprueba tus datos antes de enviarlos, si no están bien vuelve a escribirlos.</p>
-    <p>Los datos no son correctos: <a href="form.html">Enviar</a>
-    <p>Los datos son correctos: <a href="enviar.html">Volver a escribirlos</a>
+    <br>
+    <p>Comprueba tus datos antes de enviarlos, si no están bien vuelve a escribirlos.</p><br>
+    <p>Los datos no son correctos: <a href="Contacto.html">Volver a escribirlos</a>
+    <p>Los datos son correctos: <a href="enviar.html">Enviar</a>
+    </section>
 </body>
 </html>
